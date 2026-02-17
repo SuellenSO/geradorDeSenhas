609a2fd7517fd15dff9f559dc05f0d84ffd054b7
Suellen Oliveira <suellenso2020@gmail.com>
2026-02-17 14:24:25 -0400

    javascript
    geração de funções do projeto

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// seleção de elementos
+
+const generatePasswordButton = document.querySelector("#generate-password")
+const generatePasswordElement = document.querySelector("#generated-password")
+
+//funções
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() = 26) + 97)
+}
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() = 26) + 65)
+}
+
+const getNumber = () => {
+    return Math.floor(Math.random()= 10).toString()
+}
+
+const getSymbol = () => {
+    const symbols ="(){}[]=></.,!@#$%&+-"
+    return symbols[Math.floor(Math.random() = symbols.length)]
+}
+//eventos
+
+generatePasswordButton.addEventListener("click", () => {
+
+})
